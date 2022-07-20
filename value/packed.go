897a1ed1fecf6d73075689e9447138003a2485cd
Martin Lindhe <martin@ubique.se>
2022-07-20 13:20:35 +0200

    archive/xz: improve format and add vu64 data type (variable encoded u64)

--- value/packed.go
+++ value/packed.go
@@ -30,3 +30,54 @@ func ReadVariableLengthU32(r *bytes.Reader) (uint32, []byte, uint64, error) {
 	}
 	return 0, nil, 0, fmt.Errorf("exceeds 5 bytes")
 }
+
+// this encoding is used by archive/xz
+// returns decoded value, raw bytes, byte length, error
+func ReadVariableLengthU64(r *bytes.Reader) (uint64, []byte, uint64, error) {
+
+	accum := uint64(0)
+	raw := []byte{}
+
+	for i := 0; i < 9; i++ {
+		v, err := r.ReadByte()
+		if err != nil {
+			return 0, nil, 0, err
+		}
+
+		if v == 0 {
+			panic("XXX") // XXX the xz decode() example returns error here
+		}
+		raw = append(raw, v)
+		accum |= (uint64(v) & 0x7f) << (i * 7)
+
+		if v&0x80 == 0 {
+			return accum, raw, uint64(i + 1), nil
+		}
+	}
+
+	return 0, nil, 0, fmt.Errorf("exceeds 9 bytes")
+}
+
+/*
+size_t
+decode(const uint8_t buf[], size_t size_max, uint64_t *num)
+{
+	if (size_max == 0)
+		return 0;
+
+	if (size_max > 9)
+		size_max = 9;
+
+	*num = buf[0] & 0x7F;
+	size_t i = 0;
+
+	while (buf[i++] & 0x80) {
+		if (i >= size_max || buf[i] == 0x00)
+			return 0;
+
+		*num |= (uint64_t)(buf[i] & 0x7F) << (i * 7);
+	}
+
+	return i;
+}
+*/
--- value/packed_test.go
+++ value/packed_test.go
@@ -25,3 +25,21 @@ func TestReadVariableLengthU32(t *testing.T) {
 		assert.Equal(t, tst.ExpectedValue, got)
 	}
 }
+func TestReadVariableLengthU64(t *testing.T) {
+	tests := []struct {
+		Bytes          []byte
+		ExpectedLength uint64
+		ExpectedValue  uint64
+	}{
+		{[]byte{0x3f}, 1, 63},
+		{[]byte{0xa9, 0x24}, 2, 5284},
+		{[]byte{0x81, 0xe5, 0x65}, 3, 29413},
+	}
+	for _, tst := range tests {
+		r := bytes.NewReader(tst.Bytes)
+		got, _, len, err := ReadVariableLengthU64(r)
+		assert.Equal(t, nil, err)
+		assert.Equal(t, tst.ExpectedLength, len)
+		assert.Equal(t, tst.ExpectedValue, got)
+	}
+}
